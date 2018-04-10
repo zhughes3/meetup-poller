e9d49e92f3909155aec28136ce0e1a4dbaa07109
Zachary Hughes <zhughes@captechventures.com>
2018-04-10 16:24:43 -0400

    created server with events endpoint using Hapi

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
+'use strict';
+
+// libraries
 const fetch = require('node-fetch');
+const Hapi = require('hapi');
 const util = require('util');
+
+// global variables
 const obj = require('./private/creds.json');
 const MEETUP_KEY = obj.key;
 const MEETUP_API_HOST = 'https://api.meetup.com';
 const TOPIC = 'softwaredev';
 const ZIP = 43215;
 let events = [];
+
+const server = Hapi.server({
+    port: 3000,
+    host: 'localhost'
+});
+
+server.route({
+	method: 'GET',
+	path: '/events',
+	handler: (request, h) => {
+		return events;
+	}
+});
+
+const start = async () => {
+	await server.start();
+	console.log(`Server running at: ${server.info.uri}`);
+}
+
+process.on('unhandledRejection', (err) => {
+    console.log(err);
+    process.exit(1);
+});
+
+
 
 const searchGroups = () => {
 	const METHOD = '/2/groups';
@@ -25,7 +56,7 @@ const handleGroupResults = (results) => {
 		const urlname = result.urlname;
 		searchEvents(urlname)
 		.then(json => {
-			console.log('json=' + json);
+			//console.log('json=' + json);
 			json.forEach(result => {
 				events.append(result);
 			});
@@ -34,11 +65,12 @@ const handleGroupResults = (results) => {
 		});
 	});
 };
+
 // /:urlname/events
 const searchEvents = (urlname) => {
 	const METHOD = util.format('/%s/events?', urlname);
 	let url = addKey(util.format('%s%s', MEETUP_API_HOST, METHOD));
-	console.log('url = ' + url);
+	//console.log('url = ' + url);
 	return fetch(url)
 	.then(response => {
 		return response.json();
@@ -47,13 +79,15 @@ const searchEvents = (urlname) => {
 
 const addKey = (url) => {
 	return util.format('%s&sign=true&key=%s', url, MEETUP_KEY);
-}
+};
+
+start();
 
 searchGroups()
 .then(json => {
 	console.log(json);
 	let results = json.results;
-	console.log(util.format('We found %d groups with topic=%s', results.length, TOPIC));
+	//console.log(util.format('We found %d groups with topic=%s', results.length, TOPIC));
 	const meta = json.meta;
 	handleGroupResults(results);
 }).catch(err => {
